e8fbbe3786cad1bcbeb8a2af9b915ab8ce9fb325
renangeraldosilva <renangeraldosilva@gmail.com>
2022-05-04 16:11:14 -0300

    Projeto calculadora IMC

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula71_IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double peso = double.Parse(txtPeso.Text);
+            double altura = double.Parse(txtAltura.Text);
+
+            IMC imc = new IMC();
+
+            imc.Peso = peso;
+            imc.Altura = altura;
+
+            imc.ClassifImc();
+        }
+    }
+}
---
+++ IMC.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula71_IMC
+{
+    internal class IMC
+    {
+        public double Peso;
+        public double Altura;
+
+        public double CalcImc()
+        {
+            return Peso / (Altura * Altura);
+        }
+
+        public void ClassifImc()
+        {
+            if (CalcImc() < 18.5 )
+            {
+                MessageBox.Show("O Seu IMC é de: " + CalcImc().ToString("F2"));
+                MessageBox.Show("Abaixo do recomendado", "Classificação IMC");
+            }
+            else if (CalcImc() < 24.9)
+            {
+                MessageBox.Show("O Seu IMC é de: " + CalcImc().ToString("F2"));
+                MessageBox.Show("Peso normal", "Classificação IMC");
+            }
+            else if (CalcImc() < 29.9)
+            {
+                MessageBox.Show("O Seu IMC é de: " + CalcImc().ToString("F2"));
+                MessageBox.Show("Sobrepeso", "Classificação IMC");
+            }
+            else if (CalcImc() < 39.9)
+            {
+                MessageBox.Show("O Seu IMC é de: " + CalcImc().ToString("F2"));
+                MessageBox.Show("Obesidade", "Classificação IMC");
+            }
+            else
+            {
+                MessageBox.Show("O Seu IMC é de: " + CalcImc().ToString("F2"));
+                MessageBox.Show("Obesidade severa", "Classificação IMC");
+            }
+        }
+    }
+}
